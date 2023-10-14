db7629d17dd2b7493b4049b5e41f09564f617f31
Torremolinos <adrian_2054@hotmail.com>
2023-10-14 16:29:36 +0200

    Retoques html y Js

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,12 @@
 document.addEventListener("DOMContentLoaded", ()=>{
     const cambio= ()=>{
+        const titulo = document.getElementById("titulo");
         const imagen = document.getElementById("imagen");
         if(imagen.src.match(".assets/naruto.jpg")){
+            titulo.textContent= `Ten cuidao no pulses mucho`;
             imagen.src = "assets/sasuke.jpg";
         }else{
+            titulo.textContent= `Te lo dije`;
             imagen.src= "assets/kiss.jpg";
         }
     };
